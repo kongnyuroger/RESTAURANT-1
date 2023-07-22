21378da91d19a269cba767540252136352a748b2
Kongnyu Roger <kongnyuroger711@gmail.com>
2023-07-22 23:25:45 +0100

    created featured page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Footer from '@/components/Footer'
+import Featured from '@/components/Featured'
 const inter = Inter({ subsets: ['latin'] })
 
 
@@ -17,11 +18,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <Image src="/img/bike.png" alt='image'
-      width={120}
-      height={120}
-      />
+     
       
+      <Featured/>
     </>
   )
 }
